940ec8ece4e7c5d925e474c4c732a75ae2bd20d3
DomPou <45173284+DomPou@users.noreply.github.com>
2019-01-04 15:57:06 -0500

    Added variables connection

--- Batch_files_etc/Intersections_running_order.py
+++ Batch_files_etc/Intersections_running_order.py
@@ -1,4 +1,6 @@
 import sys
+sys.path.append("C:\\Trailsdb_Scripts\\Variables")
+from Connection_to_trailsdb_ArcGIS_Pro import *
 
 sys.path.append(functionsFolder)
 from trailsdb_queries_table_last_changes import intersectionsTrailsDB_queries_v1
